ae53ce49a0736d3b5870608f70a589343c049d44
msusanrbrown <sbrow120@fau.edu>
2021-07-23 01:54:52 +0000

    first commit

---
+++ src/js/component/SimpleCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+export const SimpleCounter=props=>{
+
+    return(
+        
+
+    )
+
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -9,7 +10,39 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+//import { Home } from "./component/home.js";
 
+function SecondsCounter(props) {
+	const sa = props.seconds
+		.toString()
+		.padStart(6, "0")
+		.split("");
+
+	const sa1 = sa.map((element, i) => {
+		return <div key={i}>{element}</div>;
+	});
+	return (
+		<div className="bigCounter">
+			<div className="clock">
+				<i className="far fa-clock"></i>
+			</div>
+			{sa1}
+		</div>
+	);
+}
+
+SecondsCounter.propTypes = {
+	seconds: PropTypes.number
+};
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+const start = Date.now();
+
+setInterval(() => {
+	const millis = Date.now() - start;
+	const seconds = Math.floor(millis / 1000);
+	ReactDOM.render(
+		<SecondsCounter seconds={seconds} />,
+		document.querySelector("#app")
+	);
+}, 1000);
